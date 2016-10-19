2d86709fcf881a5636087e70801f59093d0d2664
James Sibert <jsibert@basecampcodingacademy.org>
2016-10-19 13:13:03 -0500

    added shop

--- shop.py
+++ shop.py
@@ -6,8 +6,11 @@ def myshop(game=0, system=0, movie=0, pizza=0, soda=0):
     fifth = soda * 1.55
     sixth = (first + second + third + fourth + fifth)
     seventh = round((sixth * 1.07), 2)
-    print("Here is your receipt. $" + str(seventh) +
-          "\nThank you for shopping with us!")
+    if seventh > 0:
+        print("Here is your receipt. $" + str(seventh) +
+              "\nThank you for shopping with us!")
+    else:
+        print("Oh, you don't want to buy anything? Well have a good day!")
 
 
 def shop():
@@ -18,8 +21,8 @@ def shop():
           "pizza = $5.99\n"
           "soda = $1.55\n")
     myshop(
-        int(input("game: ")), int(input("system: ")), int(input("movie: ")),
-        int(input("pizza: ")), int(input("soda: ")))
+        int(input("games: ")), int(input("systems: ")), int(input("movies: ")),
+        int(input("pizzas: ")), int(input("sodas: ")))
 
 
 shop()
